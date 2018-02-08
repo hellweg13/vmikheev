9307b661838d86048581a9e6e57b24bf41b5dfe8
hellweg13 <hellweg69@gmail.com>
2018-02-08 11:58:14 +0500

    1.1. Создание Hello World[#141]

--- src/ru/job4j/Calculate.java
+++ src/ru/job4j/Calculate.java
@@ -1,6 +1,18 @@
 package ru.job4j;
 
+/**
+ * Calculate.
+ *
+ * @author Vladimir Mikheev (hellweg69@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
 public class Calculate{
+     /**
+     * Main.
+     * @param args - args.
+     */
 	public static void main (String[] args){
 		System.out.println("Hello world.");
 	}
