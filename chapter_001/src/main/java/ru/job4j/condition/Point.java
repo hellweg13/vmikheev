c22ee0ca252545825e4f99cdd868c1fe6e19a0e8
hellweg13 <hellweg69@gmail.com>
2018-02-12 20:41:51 +0500

    3.3. Вычисление площади треугольника [#9461]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -12,11 +12,11 @@ public class Point {
 	private int x;
 	private int y;
 
-	private Point(int x, int y) {
+	public Point(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
-	private double distanceTo(Point that) {
+	public double distanceTo(Point that) {
 		return Math.sqrt(Math.pow(this.x - that.x, 2)+Math.pow(this.y - that.y, 2));
 	}
 	public static void main(String[] args) {
