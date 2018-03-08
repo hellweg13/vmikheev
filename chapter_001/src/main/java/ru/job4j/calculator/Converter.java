00369147fe897428626b64274c305357be6f94df
hellweg13 <hellweg69@gmail.com>
2018-03-08 23:17:45 +0500

    Добавление констант

--- chapter_001/src/main/java/ru/job4j/calculator/Converter.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Converter.java
@@ -1,47 +1,22 @@
 package ru.job4j.calculator;
 
-/**
-* Конвертер валюты.
-*/
 public class Converter {
-	
-	/**
-	* Конвертируем рубли в евро.
-	* @param value рубли.
-	* @return Евро.
-	*/
+	public static final int euroRate = 70;
+	public static final int dollarRate = 60;
 	public int rubleToEuro(int value) {
-		int euro = value / 70;
+		final int euro = value / euroRate;
 		return euro;
 	}
-	
-	/**
-	* Конвертируем рубли в доллары.
-	* @param value рубли.
-	* @return Доллары.
-	*/
 	public int rubleToDollar(int value) {
-		int dollar = value / 60;
+		final int dollar = value / dollarRate;
 		return dollar;
 	}
-
-	/**
-	 * Конвертируем евро в рубли.
-	 * @param value евро.
-	 * @return Рубли.
-	 */
 	public int euroToRuble(int value){
-		int ruble = value * 70;
+		final int ruble = value * euroRate;
 		return ruble;
 	}
-
-	/**
-	 * Конвертируем доллары в рубли.
-	 * @param value доллары.
-	 * @return Рубли.
-	 */
 	public int dollarToRuble(int value){
-		int ruble = value * 60;
+		final int ruble = value * dollarRate;
 		return ruble;
 	}
 }
--- chapter_001/src/main/java/ru/job4j/calculator/Fit.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Fit.java
@@ -1,27 +1,15 @@
 package ru.job4j.calculator;
 
-/**
- * Программа расчета идеального веса.
- */
-
 public class Fit {
-    /**
-     * Идеальный вес для мужщины.
-     * @param height Рост.
-     * @return идеальный вес.
-     */
+    public static double ValueMan = 100;
+    public static double ValueWoman = 110;
+    public static double Const = 1.15;
     double manWeight(double height) {
-        double weight = (height -100) * 1.15;
+        double weight = (height -ValueMan) * Const;
         return weight;
     }
-
-    /**
-     * Идеальный вес для женщины.
-     * @param height Рост.
-     * @return идеальный вес.
-     */
     double womanWeight(double height) {
-        double weight = (height -110) * 1.15;
+        double weight = (height -ValueWoman) * Const;
         return weight;
     }
 }
