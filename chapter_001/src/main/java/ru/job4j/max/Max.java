b6fe28855bda58e8f08bc7e04fd6e6d72e36ed35
hellweg13 <hellweg69@gmail.com>
2018-03-11 01:06:11 +0500

    Исправления

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -5,8 +5,6 @@ public class Max {
         return first > second ? first:second;
     }
     public int max(int first, int second, int third){
-        int temp = this.max(first, second);
-        temp = this.max(temp, third);
-        return temp;
+        return this.max(this.max(first, second), third);
     }
 }
